1a5895eca0a7f870a7b08b937ecfc5677c817534
agueriv <rivasguerreroariel@gmail.com>
2023-10-17 12:18:12 +0200

    Fichero con el grueso del programa

--- PageContructor/upload.php
+++ PageContructor/upload.php
@@ -1,3 +1,20 @@
 <?php
 
-echo 'Prueba';
+require('classes/FileManager.php');
+require('classes/Files.php');
+
+$fm = new FileManager();
+// Borramos posibles archivos anteriores
+$fm->deleteFiles();
+
+// Añadimos y movemos a la carpeta files los nuevos archivos que ha subido el user
+$headTXT = $fm->send('headFile', 'files/');
+$headTXT = $fm->send('mainFile', 'files/');
+$headTXT = $fm->send('headerFile', 'files/');
+$headTXT = $fm->send('footerFile', 'files/');
+
+// Escribimos archivo final html
+$fm->writeFile();
+
+Files::redirect('sourceView.php');
+
