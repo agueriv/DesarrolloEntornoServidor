5a5ec5cb98b16e427660b72caceae34799d6a963
agueriv <rivasguerreroariel@gmail.com>
2023-10-17 17:14:44 +0200

    Codificación de la vista del código

--- PageContructor/sourceView.php
+++ PageContructor/sourceView.php
@@ -9,11 +9,14 @@
 <head>
     <meta charset="UTF-8">
     <title>Source View</title>
+    <link rel="stylesheet" href="css/styles.css" type="text/css" />
+    <link rel="shortcut icon" type="image/x-icon" href="img/ico.png">
 </head>
 <body>
     <h1>Source View</h1>
-    <pre>
-        <?= $contenido ?>
-    </pre>
+    <div class="container">
+        <?= highlight_file("files/index.html", true) ?>
+    </div>
+    <a href="files/index.html" target="_BLANK">Ver página HTML resultante</a>
 </body>
 </html>
