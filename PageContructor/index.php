9e6ee05f4a240d6ddf5437f1c3a932a243faa0e2
agueriv <rivasguerreroariel@gmail.com>
2023-10-17 17:17:54 +0200

    Adición de nombre

--- PageContructor/index.php
+++ PageContructor/index.php
@@ -4,13 +4,13 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>PageContructor</title>
+    <title>PageContructor | Ariel Guerrero</title>
     <link rel="stylesheet" href="css/styles.css">
     <link rel="shortcut icon" type="image/x-icon" href="img/ico.png">
 </head>
 
 <body>
-    <h1>PageContructor</h1>
+    <h1>PageContructor by Ariel</h1>
     <h2>Manda hasta 4 bloques de tu web y nosotros te la montamos en segundos!!</h2>
     <form action="upload.php" method="post" enctype="multipart/form-data">
         <div class="containerIn">
